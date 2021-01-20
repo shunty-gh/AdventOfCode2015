a2c528b06c8f5f15c700324a9840587ac10385ac
Steven Hunt <sph@shunty.co.uk>
2021-01-20 10:23:54 +0000

    Speed up day 18 - replace map<> with vector<>

--- src/day18.cpp
+++ src/day18.cpp
@@ -1,12 +1,7 @@
 #include <functional>
-#include <map>
 #include <string>
+#include <vector>
 #include "aoc2015.h"
-
-struct Loc {
-    int X, Y;
-};
-bool operator<(const Loc& a, const Loc& b) { return a.X == b.X ? a.Y < b.Y : a.X < b.X; }
 
 struct State {
     bool Current, Next;
@@ -15,87 +10,120 @@ struct State {
     State(bool current, bool next) { Current = current, Next = next; }
 };
 
-void prepare(std::map<Loc, State>&, bool);
-void update(std::map<Loc, State>&);
+/* Defining the grid as a vector<int> rather than a map<...> speeds up the
+   process by about 6x. We just need to take care when working out the
+   neighbours for indexes at the edges of the 'grid' - eg if current
+   index = 100 then the neigbours are
+   0, 1, 101, 200, 201 - ie NOT including 99, 199
+ */
+
+const int MaxIndex = 100 * 100;
+const int TopLeft = 0;
+const int TopRight = 99;
+const int BottomLeft = (99 * 100);
+const int BottomRight = (99 * 100) + 99;
+
+void prepare(std::vector<State>&, bool);
+void update(std::vector<State>&);
 
 aoc_result_t day18() {
-    std::map<Loc, State> grid;
-    int y = 0;
-    getInputLines(18, false, [&grid, &y](std::string line) {
+    std::vector<State> grid;
+    getInputLines(18, false, [&grid](std::string line) {
         for (int x = 0; x < (int)line.length(); x++) {
-            grid[Loc{x,y}] = State{line[x] == '#'};
+            grid.push_back(State{line[x] == '#'});
         }
-        y++;
     });
 
-    std::map<Loc, State> grid1{grid};
+    std::vector<State> grid1{grid};
     for (int i = 0; i < 100; i++) {
         prepare(grid1, false);
         update(grid1);
     }
 
     int part1 = 0;
-    for (auto&& [loc, cell] : grid1) {
+    for (auto&& cell : grid1) {
         if (cell.Current) part1++;
     }
 
-    std::map<Loc, State> grid2{grid};
-    grid2[Loc{0,0}] = State{true, true};
-    grid2[Loc{99,99}] = State{true, true};
-    grid2[Loc{0,99}] = State{true, true};
-    grid2[Loc{99,0}] = State{true, true};
+    std::vector<State> grid2{grid};
+    grid2[TopLeft] = State{true, true};
+    grid2[TopRight] = State{true, true};
+    grid2[BottomLeft] = State{true, true};
+    grid2[BottomRight] = State{true, true};
     for (int i = 0; i < 100; i++) {
         prepare(grid2, true);
         update(grid2);
     }
 
     int part2 = 0;
-    for (auto&& [loc, cell] : grid2) {
+    for (auto&& cell : grid2) {
         if (cell.Current) part2++;
     }
 
     return {part1,part2};
 }
 
-const std::vector<Loc> Neighbours{Loc{1,0}, Loc{1,1}, Loc{0,1}, Loc{-1,1}, Loc{-1,0}, Loc{-1,-1}, Loc{0,-1}, Loc{1,-1}};
+std::vector<int> get_neighbours(int index) {
+    int y = index / 100;
+    int x = index % 100;
 
-int get_active_neighbours(std::map<Loc, State>& grid, int x, int y) {
+    // Preallocate makes it much quicker than .push_back()
+    // all the time as we know max number we'll need and
+    // it gets called a lot
+    std::vector<int> result(8, -1);
+
+    if (x > 0) result[0] = index - 1;
+    if (x < 99) result[1] = index + 1;
+
+    // Add elements from 'row' above
+    if (y > 0) {
+        if (x > 0) result[2] = index - 100 - 1;
+        result[3] = index - 100;
+        if (x < 99) result[4] = index - 100 + 1;
+    }
+
+    // Add elements from 'row' below
+    if (y < 99) {
+        if (x > 0) result[5] = index + 100 - 1;
+        result[6] = index + 100;
+        if (x < 99) result[7] = index + 100 + 1;
+    }
+
+    return result;
+};
+
+int get_active_neighbours(const std::vector<State>& grid, int index, int maxOfInterest) {
     int result = 0;
-    for (auto& [dx, dy] : Neighbours) {
-        int nextx = x + dx, nexty = y + dy;
-        if (nextx < 0 || nexty < 0 || nextx >= 100 || nexty >= 100) {
-            continue;
+    for (auto& nexti : get_neighbours(index)) {
+        if (nexti >= 0) {
+            result += grid[nexti].Current == true;
+            if (result >= maxOfInterest) { // No need to continue if we've got more than we need
+                break;
+            }
         }
-        result += grid[Loc{nextx, nexty}].Current == true;
     }
     return result;
 }
 
-void prepare(std::map<Loc, State>& grid, bool part2) {
-    int maxX = 100, maxY = 100;
+void prepare(std::vector<State>& grid, bool part2) {
+    for (int i = 0; i < MaxIndex; i++) {
+        if (part2
+            && (i == TopLeft || i == TopRight || i == BottomLeft || i == BottomRight)) {
+            continue;
+        }
 
-    for (int y = 0; y < maxY; y++) {
-        for (int x = 0; x < maxX; x++) {
-            if (part2
-                && ((x == 0 && y == 0) || (x == 99 && y == 99) || (x == 0 && y == 99 ) || (y == 0 && x == 99))) {
-                continue;
-            }
+        int neighbours = get_active_neighbours(grid, i, 4);
 
-            int neighbours = get_active_neighbours(grid, x, y);
-
-            Loc loc{x,y};
-            State& current = grid[loc];
-            if (current.Current) {
-                grid[loc].Next = neighbours == 2 || neighbours == 3;
-            } else {
-                grid[loc].Next = neighbours == 3;
-            }
+        if (grid[i].Current) {
+            grid[i].Next = neighbours == 2 || neighbours == 3;
+        } else {
+            grid[i].Next = neighbours == 3;
         }
     }
 }
 
-void update(std::map<Loc, State>& grid) {
-    for (auto&& [loc, cell] : grid) {
+void update(std::vector<State>& grid) {
+    for (auto&& cell : grid) {
         cell.Current = cell.Next;
     }
 }
